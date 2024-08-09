e495938c5fe63a87b24457c56d28a8fff90b96f6
e-infotech <45136020+e-infotech@users.noreply.github.com>
2024-08-09 19:57:56 -0400

    Update main.php

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -12,11 +12,22 @@
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/posts">Posts</a></li>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <li><a href="/posts/create">Create Post</a></li>
+                    <li><a href="/logout">Logout (<?= $_SESSION['username'] ?>)</a></li>
+                <?php else: ?>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/register">Register</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
     </header>
 
     <main>
+        <?php if (isset($_SESSION['message'])): ?>
+            <p class="message"><?= $_SESSION['message']; unset($_SESSION['message']); ?></p>
+        <?php endif; ?>
+
         <?= $content ?>
     </main>
 
